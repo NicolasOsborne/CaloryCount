e5af30f10b3b783ee217da89796034d34dfb7876
Nicolas Osborne <nho.osborne@gmail.com>
2025-01-15 09:07:54 +0100

    Add Bootstrap dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import './assets/main.css'
-import 'vuetify/styles'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 import router from './router'
-
-const vuetify = createVuetify()
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
-app.use(vuetify)
 
 app.mount('#app')
